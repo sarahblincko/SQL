462ba9553626b97659df47f1bba2b102bb85c969
NHS England IAPT <98588750+NHSE-IAPT@users.noreply.github.com>
2022-01-28 12:58:45 +0000

    Update SQLQueies_101.sql

--- SQLQueies_101.sql
+++ SQLQueies_101.sql
@@ -10,15 +10,6 @@ SELECT  COUNT(DISTINCT CASE WHEN ReferralRequestReceivedDate BETWEEN @PeriodStar
 		COUNT(DISTINCT CASE WHEN ReferralRequestReceivedDate BETWEEN @PeriodStart and @PeriodEnd AND SourceOfReferralMH = 'B1' THEN PathwayID ELSE NULL END) AS Count_SelfReferrals,
 		COUNT(DISTINCT CASE WHEN ServDischDate BETWEEN @PeriodStart and @PeriodEnd AND UsePathway_Flag = 'TRUE' THEN PathwayID ELSE NULL END) AS Count_FirstAssessmentOver90days,
 		
-		-- below fields return the same value despite different parameters in the '<=' operation
-		
-		COUNT(DISTINCT CASE WHEN DATEDIFF(week,TherapySession_FirstDate, ReferralRequestReceivedDate) <= 126 THEN PathwayID ELSE NULL END) AS Count_FirstTreatment18Weeks,
-		COUNT(DISTINCT CASE WHEN DATEDIFF(week,TherapySession_FirstDate, ReferralRequestReceivedDate) <= 42 THEN PathwayID ELSE NULL END) AS Count_FirstTreatment6Weeks,
-
-		-- unsure if this is returning the correct value (but at least it doesn;t throw an error!!)
-		
-		AVG(DISTINCT CASE WHEN ServDischDate BETWEEN @PeriodStart and @PeriodEnd AND UsePathway_Flag = 'TRUE' THEN PathwayID ELSE NULL END) AS Mean_WaitFinishedCourseTreatment
-
 FROM [NHSE_IAPT_v2].dbo.IDS101_Referral r
 INNER JOIN [NHSE_IAPT_v2].[dbo].[IsLatest_SubmissionID] l ON r.[UniqueSubmissionID] = l.[UniqueSubmissionID] AND r.AuditId = l.AuditId
 INNER JOIN [NHSE_IAPT_v2].[dbo].[IDS000_Header] h ON r.[UniqueSubmissionID] = h.[UniqueSubmissionID]

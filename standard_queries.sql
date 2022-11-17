66fdd2a905fb3ffbbb9042643affe0b09bbba9d0
alxmacdnld <alxmacdnld@gmail.com>
2022-11-17 16:22:59 +0000

    updated formatting

--- standard_queries.sql
+++ standard_queries.sql
@@ -1,3 +1,6 @@
+
+-- an @Offset of '0' returns values from the most recent month of data in NCDR (primary submission)
+
 DECLARE @Offset AS INT = -1
 
 DECLARE @PeriodStart AS DATE = (SELECT DATEADD(MONTH,@Offset,MAX([ReportingPeriodStartDate])) FROM [NHSE_IAPT_v2].[dbo].[IDS000_Header])
@@ -12,7 +15,7 @@ SELECT  @MonthYear AS 'Month',
 		COUNT(DISTINCT CASE WHEN ReferralRequestReceivedDate BETWEEN @PeriodStart AND @PeriodEnd AND SourceOfReferralIAPT = 'B1' THEN PathwayID ELSE NULL END) AS 'Count_SelfReferrals',
 		COUNT(DISTINCT CASE WHEN ServDischDate BETWEEN @PeriodStart AND @PeriodEnd AND UsePathway_Flag = 'TRUE' THEN PathwayID ELSE NULL END) AS 'Count_FirstAssessmentOver90days'
 
-FROM    [NHSE_IAPT_v2].[dbo].[IDS101_Referral] r
+FROM    [NHSE_IAPT_v2].dbo.IDS101_Referral r
 		------------------------------------
         INNER JOIN [NHSE_IAPT_v2].[dbo].[IsLatest_SubmissionID] l ON r.[UniqueSubmissionID] = l.[UniqueSubmissionID] AND r.AuditId = l.AuditId
         INNER JOIN [NHSE_IAPT_v2].[dbo].[IDS000_Header] h ON r.[UniqueSubmissionID] = h.[UniqueSubmissionID]

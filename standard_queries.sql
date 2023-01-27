befb02f4197dd0d13c964307703d02f18fa9eda8
Kaz Khundakar <77837651+KKhundakar@users.noreply.github.com>
2023-01-27 09:22:19 +0000

    Update standard_queries.sql

--- standard_queries.sql
+++ standard_queries.sql
@@ -9,16 +9,13 @@ DECLARE @PeriodEnd AS DATE = (SELECT EOMONTH(DATEADD(MONTH,@Offset,MAX([Reportin
 DECLARE @MonthYear AS VARCHAR(50) = (DATENAME(M, @PeriodStart) + ' ' + CAST(DATEPART(YYYY, @PeriodStart) AS VARCHAR))
 
 SELECT  @MonthYear AS 'Month',
-		COUNT(DISTINCT CASE WHEN ReferralRequestReceivedDate BETWEEN @PeriodStart AND @PeriodEnd THEN PathwayID ELSE NULL END) AS 'Count_IAPTReferrals',
+		COUNT(DISTINCT CASE WHEN ReferralRequestReceivedDate BETWEEN @PeriodStart AND @PeriodEnd THEN PathwayID ELSE NULL END) AS 'Count_Referrals',
 		COUNT(DISTINCT CASE WHEN TherapySession_FirstDate BETWEEN @PeriodStart AND @PeriodEnd THEN PathwayID ELSE NULL END) AS 'Count_FirstTreatment',
-		COUNT(DISTINCT CASE WHEN ServDischDate BETWEEN @PeriodStart AND @PeriodEnd AND CompletedTreatment_Flag = 'TRUE' THEN PathwayID ELSE NULL END) AS 'Count_EndedReferrals',
-		COUNT(DISTINCT CASE WHEN ReferralRequestReceivedDate BETWEEN @PeriodStart AND @PeriodEnd AND SourceOfReferralIAPT = 'B1' THEN PathwayID ELSE NULL END) AS 'Count_SelfReferrals',
-		COUNT(DISTINCT CASE WHEN ServDischDate BETWEEN @PeriodStart AND @PeriodEnd AND UsePathway_Flag = 'TRUE' THEN PathwayID ELSE NULL END) AS 'Count_FirstAssessmentOver90days'
+		COUNT(DISTINCT CASE WHEN ServDischDate BETWEEN @PeriodStart AND @PeriodEnd AND CompletedTreatment_Flag = 'TRUE' THEN PathwayID ELSE NULL END) AS 'Count_FinishedCourseTreatment'
 
 FROM    [NHSE_IAPT_v2].dbo.IDS101_Referral r
 		------------------------------------
         INNER JOIN [NHSE_IAPT_v2].[dbo].[IsLatest_SubmissionID] l ON r.[UniqueSubmissionID] = l.[UniqueSubmissionID] AND r.AuditId = l.AuditId
-        INNER JOIN [NHSE_IAPT_v2].[dbo].[IDS000_Header] h ON r.[UniqueSubmissionID] = h.[UniqueSubmissionID]
 
 WHERE   UsePathway_Flag = 'True' AND IsLatest = 1 
 		AND h.[ReportingPeriodStartDate] BETWEEN @PeriodStart AND @PeriodEnd
